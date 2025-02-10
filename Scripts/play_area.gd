406260118c521bd6159c1f6d2de6d7a0f787f65b
Brian Piert <SucellosMusic@gmail.com>
2025-02-09 21:39:49 -0500

    playable prototype ready
    fixed collisions, added scoring functions (still need to add multiplier), slowly adding features to streamline creating tracks

--- Scripts/play_area.gd
+++ Scripts/play_area.gd
@@ -29,9 +29,12 @@ var selection : int = 0
 var prevSelection : int = 0
 
 #scoring
-@onready var posPoint : int = 0
-@onready var orientPoint : int = 0
-@onready var timePoint : int = 0
+@onready var scoreDisplay : Label = $"Score Int"
+var posPoint : int = 0
+var orientPoint : int = 0
+var timePoint : int = 0
+var totalScore : int = 0
+var takeScore : bool = false
 
 
 func _ready() ->void:
@@ -48,16 +51,39 @@ func _ready() ->void:
 	beatTimer.start(syncToStart + .1)
 
 func get_score() ->void:
-	posPoint = get_pos_score()
-		
-
-
+	if takeScore == true:
+		posPoint = get_pos_score()
+		orientPoint = get_orient_score()
+		timePoint = get_time_score()
+		totalScore += posPoint + orientPoint + timePoint
+		takeScore = false
 
 func get_pos_score() -> int:
+	if shapePerf[selection].has_overlapping_areas():
+		return 3
+	elif shapeGreat[selection].has_overlapping_areas():
+		return 2
+	elif shapeOkay[selection].has_overlapping_areas():
+		return 1
 	return 0
 
 func get_orient_score() -> int:
+	if shapeOkay[selection].has_overlapping_areas():
+		if shapes[selection].rotation == receivers[selection].rotation:
+			return 1
 	return 0
+	
+
+func get_time_score() -> int:
+	if shapeOkay[selection].has_overlapping_areas():
+		if beatTimer.time_left > 0 && beatTimer.time_left < .02:
+			return 3
+		elif beatTimer.time_left > .02 && beatTimer.time_left < .05:
+			return 2
+		elif beatTimer.time_left > .05 && beatTimer.time_left < .1:
+			return 1
+	return 0
+
 
 func next_shape() -> void:
 	prevSelection = selection
@@ -68,43 +94,46 @@ func next_shape() -> void:
 
 func _on_in_timer_timeout() -> void:
 	beatTimer.start(beats)
-	if halfBeat == 2:
-		halfBeat = 1
-		beat += 1
-		pathFollow.progress += 50
-	else:
-		halfBeat += 1
 	
-	if beat == 5:
-		beat = 1
+	if beat == 2 || beat == 4:
+		takeScore = true
+		pathFollow.progress += 50
+		next_shape()
+	
+	if beat == 4:
 		bar += 1
-
+		beat = 1
+	else: 
+		beat += 1
+	
+	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_A:
 			get_score()
+			scoreDisplay.text = str(totalScore)
 
 
 func _process(_float) -> void:
 	match beat:
 		1:
-			receivers[3].position = receiverSTORAGE
-			shapes[3].position = shapeSTORAGE
-			receivers[0].position = receiverPos.global_position
-			shapes[0].position = get_local_mouse_position()
+			receivers[prevSelection].position = receiverSTORAGE
+			shapes[prevSelection].position = shapeSTORAGE
+			receivers[selection].position = receiverPos.global_position
+			shapes[selection].position = get_local_mouse_position()
 		2:
-			shapes[0].position = shapeSTORAGE
-			receivers[0].position = receiverSTORAGE
-			receivers[1].position = receiverPos.global_position
-			shapes[1].position = get_local_mouse_position()
+			shapes[prevSelection].position = shapeSTORAGE
+			receivers[prevSelection].position = receiverSTORAGE
+			receivers[selection].position = receiverPos.global_position
+			shapes[selection].position = get_local_mouse_position()
 		3:
-			shapes[1].position = shapeSTORAGE
-			receivers[1].position = receiverSTORAGE
-			receivers[2].position = receiverPos.global_position
-			shapes[2].position = get_local_mouse_position()
+			shapes[prevSelection].position = shapeSTORAGE
+			receivers[prevSelection].position = receiverSTORAGE
+			receivers[selection].position = receiverPos.global_position
+			shapes[selection].position = get_local_mouse_position()
 		4:
-			shapes[2].position = shapeSTORAGE
-			receivers[2].position = receiverSTORAGE
-			receivers[3].position = receiverPos.global_position
-			shapes[3].position = get_local_mouse_position()
+			shapes[prevSelection].position = shapeSTORAGE
+			receivers[prevSelection].position = receiverSTORAGE
+			receivers[selection].position = receiverPos.global_position
+			shapes[selection].position = get_local_mouse_position()
