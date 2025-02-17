706da29cf591ac1c50dc9dfc2b6edf0c4287c9a7
Brian Piert <SucellosMusic@gmail.com>
2025-02-16 19:19:00 -0500

    Hints
    added hint sprites and sequence

--- Scripts/play_area.gd
+++ Scripts/play_area.gd
@@ -1,28 +1,31 @@
 extends Node2D
 
-#button
-@onready var startButton : Button = $Button
-@onready var instructions : Label = $Label
-@onready var canStart : bool = false
-
 #Audio
 @onready var audioPlayer : AudioStreamPlayer = $AudioStreamPlayer
 
 #shapes and receivers
 @onready var shapes = $PlayerShapes.get_children()
 @onready var receivers = $Receivers.get_children()
+@onready var altReceivers = $"Alt Receivers".get_children()
+@onready var hints = $Hints.get_children()
 
 #path
 @onready var pathFollow : PathFollow2D = $"Path2D/PathFollow2D"
+@onready var pathFollowTwo : PathFollow2D = $Path2D/PathFollow2D2
 @onready var receiverPos : Node2D = $"Path2D/PathFollow2D/Receiver Positioner"
+@onready var hintPos : Node2D = $"Path2D/PathFollow2D2/Hint Positioner"
 
 #Sequences
 @export_range(0, 6) var receiverPattern : Array[int];
-@export_range(-90, 180, 45) var receiverOrientation : Array[int]
-var selection : int;
+@export_range(-135, 180, 45) var receiverOrientation : Array[int]
+var hintSelection : int = 1
+var selection : int = 0
 var currSelection : int;
 var prevSelection : int;
 var currRotation : int = 0
+var hintPrevSelection : int;
+var hintCurrSelection : int;
+var hintRotation : int;
 
 #collision detectors
 var shapePerf : Array[Area2D] = []
@@ -34,11 +37,15 @@ var shapeOkay : Array[Area2D] = []
 @export var bpm : float;
 @onready var beats : float = (1/bpm)*60
 @onready var beatTimer : Timer = $Beats
+@export var beatOffset : float;
 
 #misc var
 @onready var shapeSTORAGE = Vector2(-500, 400)
 @onready var receiverSTORAGE = Vector2(-500, 150)
+@onready var altReceiverStorage = Vector2(-500, 650)
+@onready var hintsStorage = Vector2(-500, 900)
 var recAnimate : Array[AnimatedSprite2D] = []
+var altRecAnimate : Array[AnimatedSprite2D] = []
 
 #scoring
 @onready var scoreDisplay : Label = $"Score Int"
@@ -59,18 +66,21 @@ func _ready() ->void:
 	for r in receivers:
 		r.position = receiverSTORAGE
 		recAnimate.append((r.get_child(0)))
+		
+	for ar in altReceivers:
+		ar.position = altReceiverStorage
+		altRecAnimate.append((ar.get_child(0)))
+		
 	
 	for ra in recAnimate:
 		ra.play()
+		
+	for ara in altRecAnimate:
+		ara.play()
 
-
-func _on_button_pressed() -> void:
-	canStart = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	audioPlayer.play()
-	beatTimer.start(syncToStart + .1)
-	startButton.hide()
-	instructions.hide()
+	beatTimer.start(syncToStart + beatOffset)
 	
 
 func get_score() -> void:
@@ -82,56 +92,72 @@ func get_score() -> void:
 		takeScore = false
 
 func get_pos_score() -> int:
-	if shapePerf[selection].has_overlapping_areas():
+	if shapePerf[currSelection].has_overlapping_areas():
 		return 3
-	elif shapeGreat[selection].has_overlapping_areas():
+	elif shapeGreat[currSelection].has_overlapping_areas():
 		return 2
-	elif shapeOkay[selection].has_overlapping_areas():
+	elif shapeOkay[currSelection].has_overlapping_areas():
 		return 1
 	return 0
 
 func get_orient_score() -> int:
-	if shapeOkay[selection].has_overlapping_areas():
-		if shapes[selection].rotation == receivers[selection].rotation:
+	if shapeOkay[currSelection].has_overlapping_areas():
+		if shapes[currSelection].rotation == receivers[currSelection].rotation:
 			return 1
 	return 0
 
 func get_time_score() -> int:
-	if shapeOkay[selection].has_overlapping_areas():
-		if beatTimer.time_left > 0 && beatTimer.time_left < .02:
+	if shapeOkay[currSelection].has_overlapping_areas():
+		if beatTimer.time_left > 0 && beatTimer.time_left < .2:
 			return 3
-		elif beatTimer.time_left > .02 && beatTimer.time_left < .05:
+		elif beatTimer.time_left > .3 && beatTimer.time_left < .5:
 			return 2
-		elif beatTimer.time_left > .05 && beatTimer.time_left < .1:
+		elif beatTimer.time_left > 6 && beatTimer.time_left < 1:
 			return 1
 	return 0
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_A:
-			get_score()
-			scoreDisplay.text = str(totalScore)
-
 func next_shape() -> void:
+	hintPrevSelection = hintCurrSelection
 	prevSelection = currSelection
 	if selection == 6:
 		selection = 0
 	else:
 		selection += 1
+		
+	if hintSelection == 6:
+		hintSelection = 0
+	else:
+		hintSelection += 1
+	
+	
 	currSelection = receiverPattern[selection]
 	currRotation = receiverOrientation[selection]
+	hintCurrSelection = receiverPattern[hintSelection]
+	hintRotation = receiverOrientation[hintSelection]
+	
+	
 
 func _on_in_timer_timeout() -> void:
 	beatTimer.start(beats)
 	next_shape()
 	takeScore = true
 	pathFollow.progress += 50
+	pathFollowTwo.progress += 50
 
 func _process(delta: float) -> void:
-	if canStart:
-		receivers[prevSelection].position = receiverSTORAGE
-		shapes[prevSelection].position = shapeSTORAGE
-		receivers[currSelection].position = receiverPos.global_position
-		receivers[currSelection].rotation_degrees = currRotation
-		shapes[currSelection].position = get_local_mouse_position()
+	hints[hintPrevSelection].position = hintsStorage
+	receivers[prevSelection].position = receiverSTORAGE
+	shapes[prevSelection].position = shapeSTORAGE
+	hints[hintCurrSelection].position = hintPos.global_position
+	hints[hintCurrSelection].rotation_degrees = hintRotation
+	receivers[currSelection].position = receiverPos.global_position
+	receivers[currSelection].rotation_degrees = currRotation
+	shapes[currSelection].position = get_local_mouse_position()
 	
+	if Input.is_action_just_pressed("letter_A"):
+		get_score()
+		scoreDisplay.text = str(totalScore)
+		
+	if !audioPlayer.playing:
+		await get_tree().create_timer(3).timeout
+		get_tree().quit()
