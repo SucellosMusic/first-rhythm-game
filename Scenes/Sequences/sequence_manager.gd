b9a988579f9cf3f1cfe44f62d0f14d320a7be766
Brian Piert <SucellosMusic@gmail.com>
2025-03-11 08:27:37 -0400

    Sequence Manager continued
    building out sequence manager

--- Scenes/Sequences/sequence_manager.gd
+++ Scenes/Sequences/sequence_manager.gd
@@ -3,6 +3,7 @@ extends Node
 #sequences
 @onready var sequences = self.get_children();
 var currSequence : int = 0;
+var currSequenceValues : int = 0;
 
 #area2d
 @onready var altRec = $"../Alt Receivers".get_children()
@@ -19,13 +20,28 @@ func _ready() -> void:
 		collider.append(a.get_child(1))
 
 func set_current_sequence(selection : Area2D) -> void:
-	pass
+	for c in collider:
+		if c.has_overlapping_areas():
+			if currSequence >= self.get_child_count() - 2:
+				currSequence = 0
+			else:
+				currSequence += 2
+		else:
+			if currSequence == self.get_child_count():
+				currSequence = 0
+			else:
+				currSequence += 1
 
-func set_shape_values() -> void:
-	pass
+func zero_values() -> void:
+	currSequenceValues = 0
+	
+func increment_values() -> void:
+	currSequenceValues += 1
 
-func set_initial_position() -> void:
-	pass
+func set_initial_positions() -> void:
+	for s in sequences:
+		s.recPosSetter.progress = s.posStart
+		s.hintPosSetter.progress = s.hintStart
 
 func set_next_position() -> void:
-	pass
+	sequences[currSequence].recPosSetter.progress += sequences[currSequence].incrementValue
